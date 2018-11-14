51b9128f6ed2e1bf5bd921b129b1d23b7ef120a9
rcardin <rc15/03I>
2018-11-14 16:04:59 +0100

    Beta reduction on Stocks type

--- src/main/scala/org/rcardin/monad/state/StocksApp.scala
+++ src/main/scala/org/rcardin/monad/state/StocksApp.scala
@@ -2,28 +2,56 @@ package org.rcardin.monad.state
 
 object StocksApp {
 
+  val Prices: Map[String, Double] = Map("AMZN" -> 1631.17, "GOOG" -> 1036.05, "TSLA" -> 346.00)
+
   /**
     * A stocks portfolio, which associate a stock name to the quantity owned
     */
   type Stocks = Map[String, Double]
 
   /**
-    * Buys an amount (dollars) of the stock with given {@code name}. Returns the number
+    * Buys an amount (dollars) of the stock with given `name`. Returns the number
     * of purchased stocks.
+    *
     * @param name The name of the stocks to buy
     * @param amount The amount in dollars to buy
     * @param portfolio The portfolio to use
     * @return The quantity of stocks purchased
     */
-  def buy(name: String, amount: Double, portfolio: Stocks): (Double, Stocks) = ???
+  def buy(name: String, amount: Double)(portfolio: Stocks): (Double, Stocks) = {
+    val purchased = amount / Prices(name)
+    val owned = portfolio(name)
+    (purchased, portfolio + (name -> (owned + purchased)))
+  }
 
   /**
-    * Sells a {@code quantity} of stocks of the given {@code name}. Returns the amount of
+    * Sells a `quantity` of stocks of the given `name`. Returns the amount of
     * dollars earned by the selling operation.
+    *
     * @param name The name of the stocks to sell
     * @param quantity The quantity of stocks to sell
     * @param portfolio The portfolio to use
     * @return The earned amount
     */
-  def sell(name: String, quantity: Double, portfolio: Stocks): (Double, Stocks) = ???
+  def sell(name: String, quantity: Double)(portfolio: Stocks): (Double, Stocks) = {
+    val revenue = quantity * Prices(name)
+    val owned = portfolio(name)
+    (revenue, portfolio + (name -> (owned - quantity)))
+  }
+
+  /**
+    * Sells all stocks called `from`, and with the revenue buys stocks called `to`.
+    * Returns the quantity of stock sold and the quantity of stocks purchased.
+    *
+    * @param from Stocks to be sold
+    * @param to Stocks to be purchased
+    * @param portfolio The portfolio to use
+    * @return The quantity of stock sold and the quantity of stocks purchased
+    */
+  def move(from: String, to: String)(portfolio: Stocks): ((Double, Double), Stocks) = {
+    val originallyOwned = portfolio(from)
+    val (revenue, newPortfolio) = sell(from, originallyOwned)(portfolio)
+    val (purchased, veryNewPortfolio) = buy(to, revenue)(newPortfolio)
+    ((originallyOwned, purchased), veryNewPortfolio)
+  }
 }
